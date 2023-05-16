17876138c1638db3f49c33738c8f4ac3ae5fb4d7
Casey Quinn <74877883+cquinn2020@users.noreply.github.com>
2023-05-16 13:47:16 -0500

    Started doing authentication for the application and researched Spring Security.

--- src/main/java/com/caseyquinn/userservice/config/SecurityConfig.java
+++ src/main/java/com/caseyquinn/userservice/config/SecurityConfig.java
@@ -1,22 +1,47 @@
+
 package com.caseyquinn.userservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.caseyquinn.userservice.service.UserDetailsServiceImpl;
 
 @Configuration
 public class SecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/users/register").permitAll() // All I understand is that this is a security
-                                                                        // filter chain that allows all requests to the
-                                                                        // /users/register endpoint
-                        .anyRequest().authenticated());
+                        .requestMatchers("/users/register").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin().and()
+                .logout();
+
+        http.authenticationProvider(authenticationProvider());
+
         return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
     }
 }
---
+++ src/main/java/com/caseyquinn/userservice/service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.caseyquinn.userservice.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.caseyquinn.userservice.model.User;
+import com.caseyquinn.userservice.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+}
